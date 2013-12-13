86b22df1833f9dfb1e3c87ab7a7ff4aad9db8b61
Cesar Zegarra <czegarram@gmail.com>
2013-12-13 12:41:55 -0500

    Validacion Mant. Categoria

--- src/main/java/org/ruqu/ras/beans/CategoriaBean.java
+++ src/main/java/org/ruqu/ras/beans/CategoriaBean.java
@@ -39,6 +39,131 @@ public class CategoriaBean implements Serializable{
 	private boolean accionEditar = true;
 
 
+	
+
+ 	/* CONSTRUCTOR AND POSTCONSTRUCT 
+	*  ============================
+ 	*/
+	
+	public CategoriaBean(){
+		categorias=new ArrayList<Categoria>();
+	}
+	
+	@PostConstruct
+	public void init(){
+		categorias=getCategoriaService().getCategorias();
+	}
+	
+	/* AJAX BUTTON EVENTS  
+	*  ==================
+	*/
+
+	
+	public void nuevoEvent(){
+		accionEditar=false;
+		limpiarCampos();
+	}
+	
+	public void editarEvent(){
+		accionEditar=true;
+		try{
+			if(getCategoriaNuevo()!=null){
+				setCategoria(getCategoriaService().getCategoriaById(getCategoriaNuevo().getIdCategoria()));
+				RequestContext.getCurrentInstance().execute("dialog.show()");
+			}
+			else{
+				FacesMessageHelper.sendGrowlMessage(FacesMessage.SEVERITY_WARN, "Error", "Seleccione un registro", growlPath);
+			}
+		}catch(Exception e){
+			System.out.println("ERROR: "+e.getMessage());
+			accionEditar=false;
+		}
+	}
+	
+	public void eliminarEvent(){
+		if(getCategoriaNuevo()!=null){
+			RequestContext.getCurrentInstance().execute("confirmation.show()");
+		}
+		else{
+			FacesMessageHelper.sendGrowlMessage(FacesMessage.SEVERITY_WARN, "Error", "Seleccione un registro", growlPath);
+		}
+	}
+	
+	public void onEdit(RowEditEvent event) {
+		Categoria c=(Categoria)event.getObject();				
+		setCategoria(c);		
+		editar();
+		FacesMessageHelper.sendGrowlMessage(FacesMessage.SEVERITY_INFO, "Edicion Exitosa", ((Categoria) event.getObject()).getNombre(), growlPath);
+    }  
+      
+    public void onCancel(RowEditEvent event) {        
+        FacesMessageHelper.sendGrowlMessage(FacesMessage.SEVERITY_INFO, "Edicion Cancelada", ((Categoria) event.getObject()).getNombre(), growlPath);
+    }
+    
+	//BOTON PROCESAR-DIALOG
+	public void procesarDialog(){
+		if(accionEditar){			
+			editar();
+		}else{
+			insertar();
+		}
+		limpiarCampos();
+		refrescarCategorias();
+		RequestContext.getCurrentInstance().execute("dialog.hide();");
+	}
+	
+	/* ACCIONES CRUD
+	*  =============
+	*/
+	
+	public void insertar(){
+		getCategoriaService().addCategoria(getCategoria());		
+	}
+	
+	public void editar(){		
+		getCategoriaService().updateCategoria(getCategoria());		
+	}
+	
+	public void eliminar(){
+		getCategoriaService().deleteCategoriaLogico(getCategoriaNuevo());		
+	}
+	
+	
+	
+	public void validarEliminar(){
+		 
+        
+        if(getCategoriaNuevo()!=null){
+        	FacesMessageHelper.sendGrowlMessage(FacesMessage.SEVERITY_INFO, "Eliminado", getCategoriaNuevo().getNombre(), growlPath);
+            eliminar();
+            refrescarCategorias();
+        } else {  
+        	FacesMessageHelper.sendGrowlMessage(FacesMessage.SEVERITY_WARN, "Error", "Seleccione un registro", growlPath);
+   
+        }          
+
+	}
+	/*
+	*	RUTINAS ADICIONALES
+	*	===================
+    */
+	public void showError(){
+		FacesMessageHelper.sendGrowlMessage(FacesMessage.SEVERITY_WARN, "Registro","Campos resaltados invalidos", growlPath
+				,"Error");		
+	}
+
+	public void limpiarCampos()
+	{
+		categoria= new Categoria();		
+		categoriaNuevo = new Categoria();
+	}
+
+	private void refrescarCategorias()
+	{
+		setCategorias(getCategoriaService().getCategorias());
+		
+	}
+	
 	/* SETTER AND GETTERS
 	*  ==================
 	*/
@@ -93,185 +218,5 @@ public class CategoriaBean implements Serializable{
 		}
 	}
 
-
- 	/* CONSTRUCTOR AND POSTCONSTRUCT 
-	*  ============================
- 	*/
-	
-	public CategoriaBean(){
-		categorias=new ArrayList<Categoria>();
-	}
-	
-	@PostConstruct
-	public void init(){
-		categorias=getCategoriaService().getCategorias();
-	}
-	
-	/* AJAX BUTTON EVENTS  
-	*  ==================
-	*/
-
-	
-	public void nuevoEvent(){
-		accionEditar=false;
-		limpiarCampos();
-	}
-	
-	public void editarEvent(){
-		accionEditar=true;
-		try{
-			if(getCategoriaNuevo()!=null){
-				setCategoria(getCategoriaService().getCategoriaById(getCategoriaNuevo().getIdCategoria()));
-				RequestContext.getCurrentInstance().execute("dialog.show()");
-			}
-			else{
-				FacesMessage msg = null;  
-				msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error", "Seleccione un registro");  
-				FacesContext.getCurrentInstance().addMessage(null, msg);
-			}
-		}catch(Exception e){
-			System.out.println("ERROR: "+e.getMessage());
-			accionEditar=false;
-		}
-	}
-	
-	public void eliminarEvent(){
-		if(getCategoriaNuevo()!=null){
-			RequestContext.getCurrentInstance().execute("confirmation.show()");
-		}
-		else{
-			FacesMessage msg = null;  
-			msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error", "Seleccione un registro");  
-			FacesContext.getCurrentInstance().addMessage(null, msg);
-		}
-	}
-	
-	public void onEdit(RowEditEvent event) {
-		Categoria c=(Categoria)event.getObject();
-		setCategoria(categoriaService.getCategoriaById(c.getIdCategoria()));
-		
-		getCategoria().setNombre(c.getNombre());
-		getCategoria().setDescripcion(c.getDescripcion());
-		
-		System.out.println(c.getNombre());
-		validarEditar();
-		
-        FacesMessage msg = new FacesMessage("Categoria Editado", ((Categoria) event.getObject()).getNombre());  
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }  
-      
-    public void onCancel(RowEditEvent event) {  
-        FacesMessage msg = new FacesMessage("Categoria Cancelado", ((Categoria) event.getObject()).getNombre());  
-  
-        FacesContext.getCurrentInstance().addMessage(null, msg);  
-    }
-	//BOTON PROCESAR-DIALOG
-	public void procesarDialog(){
-		if(accionEditar){
-			System.out.println("\n\n\n\nllegue");
-			validarEditar();
-		}else{
-			validarNuevo();
-		}
-		RequestContext.getCurrentInstance().execute("dialog.hide();");
-		refrescarCategorias();
-	}
-	
-	/* ACCIONES CRUD
-	*  =============
-	*/
-	
-	public void insertar(){
-		getCategoriaService().addCategoria(getCategoria());
-		refrescarCategorias();
-		limpiarCampos();
-	}
-	
-	public void editar(){
-		//getCategoria().setIdCategoria(getCategoria().getIdCategoria());
-		getCategoriaService().updateCategoria(getCategoria());
-		refrescarCategorias();
-		limpiarCampos();
-	}
-	
-	public void eliminar(){
-		getCategoriaService().deleteCategoriaLogico(getCategoriaNuevo());
-		refrescarCategorias();
-	}
-	
-	public void validarNuevo(){
-		RequestContext context = RequestContext.getCurrentInstance();  
-        FacesMessage msg = null;  
-        boolean registrado = false;  
-          
-        if(getCategoria().getNombre()!=null){
-        	registrado=true;
-        	msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Registrado", getCategoria().getNombre());
-            insertar();
-        } else {  
-        	registrado = false;  
-            msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error de registro", "Campo(s) invalido(s)");  
-        }
-        FacesContext.getCurrentInstance().addMessage(null, msg);  
-        context.addCallbackParam("registrado", registrado);
-	}
-	
-	public void validarEditar(){
-		RequestContext context = RequestContext.getCurrentInstance();  
-        FacesMessage msg = null;  
-        boolean editado = false;  
-        
-        if(getCategoria().getNombre()!=null){
-        	editado=true;
-        	msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Editado", getCategoria().getNombre());
-            editar();
-        }else if(getCategoriaNuevo()==null){
-        	editado = false;  
-            msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error", "Seleccione un registro");  
-        }else{
-        	editado = false;  
-            msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error", "Nombre invalido");  
-        }
-        FacesContext.getCurrentInstance().addMessage(null, msg);  
-        context.addCallbackParam("editado", editado);  
-	}
-	
-	public void validarEliminar(){
-		RequestContext context = RequestContext.getCurrentInstance();  
-        FacesMessage msg = null;  
-        boolean eliminado = false;  
-        
-        if(getCategoriaNuevo()!=null){
-        	eliminado = true;  
-            msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Eliminado", getCategoriaNuevo().getNombre());
-            eliminar();
-        } else {  
-        	eliminado = false;  
-            msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Error", "Seleccione un registro");  
-        }  
-          
-        FacesContext.getCurrentInstance().addMessage(null, msg);  
-        context.addCallbackParam("eliminado", eliminado);  
-	}
-	/*
-	*	RUTINAS ADICIONALES
-	*	===================
-    */
-	public void showError(){
-		FacesMessageHelper.sendGrowlMessage(FacesMessage.SEVERITY_WARN, "Registro","Campos resaltados invalidos", growlPath
-				,"Error");		
-	}
-
-	public void limpiarCampos()
-	{
-		categoria= new Categoria();		
-		categoriaNuevo = new Categoria();
-	}
-
-	private void refrescarCategorias()
-	{
-		setCategorias(getCategoriaService().getCategorias());
-		
-	}
 
 }
